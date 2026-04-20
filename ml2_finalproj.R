6b3f2a3bdd5824992494495a0e89b86a55df5964
Alicia <master.of.wolves44@gmail.com>
2026-04-20 09:59:50 -0500

    Update ml2_finalproj.R

--- ml2_finalproj.R
+++ ml2_finalproj.R
@@ -8,7 +8,7 @@ char_indices <- which(sapply(hotel_data, is.character))
 hotel_data[, char_indices] <- lapply(hotel_data[, char_indices], as.factor)
 str(hotel_data)
 
-# Reorder factors so that not cancelled is basline.
+# Reorder factors so that not cancelled is baseline.
 hotel_data$booking.status <- relevel(hotel_data$booking.status, ref = "Not_Canceled")
 levels(hotel_data$booking.status)
 
@@ -21,6 +21,24 @@ train_data <- hotel_data[train_indices, ]
 test_data  <- hotel_data[-train_indices, ]
 
 
+# Random Forest
+library(ranger)
+
+rf_mod <- ranger(booking.status ~ ., 
+                 data = train_data, 
+                 num.trees = 500,
+                 importance = "impurity",
+                 classification = TRUE)
+
+print(rf_mod)
+
+importance_scores <- sort(rf_mod$variable.importance, decreasing = TRUE)
+print(importance_scores)
+barplot(importance_scores, las = 2, main = "Variable Importance (RF)")
+
+rf_preds <- predict(rf_mod, data = test_data)$predictions
+mean(rf_preds != test_data$booking.status)
+
 # Elastic Net
 
 X <- model.matrix(booking.status ~ ., data = hotel_data)[, -1]
@@ -28,3 +46,40 @@ y <- hotel_data$booking.status
 
 library(glmnet)
 fit_stable <- cv.glmnet(X, y, family = "binomial", alpha = 0.5)
+
+
+library(mgcv)
+
+X_test <- model.matrix(booking.status ~ ., data = test_data)[, -1]
+y_test <- test_data$booking.status
+en_preds <- predict(fit_stable, newx = X_test, s = "lambda.min", type = "class")
+
+# Convert to factor
+en_preds <- factor(en_preds, levels = levels(y_test))
+mean(en_preds != y_test)
+
+# GAM
+gam_mod <- gam(booking.status ~ s(lead.time) + s(average.price) + 
+                 type.of.meal + market.segment.type + special.requests, 
+               data = train_data, 
+               family = "binomial")
+
+
+
+summary(gam_mod)
+
+plot(gam_mod, pages = 1)
+
+
+gam_probs <- predict(gam_mod, newdata = test_data, type = "response")
+
+gam_preds <- ifelse(gam_probs > 0.5, "Canceled", "Not_Canceled")
+gam_preds <- factor(gam_preds, levels = levels(test_data$booking.status))
+
+mean(gam_preds != test_data$booking.status)
+
+
+
+
+
+
