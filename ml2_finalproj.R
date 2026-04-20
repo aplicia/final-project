396d35cf0b0ceda06452b80c324f04f5cf85907c
Alicia <master.of.wolves44@gmail.com>
2026-04-19 21:43:44 -0500

    Update ml2_finalproj.R

--- ml2_finalproj.R
+++ ml2_finalproj.R
@@ -1,1 +1,13 @@
 dat <- read.csv('booking.csv')
+#Remove booking ID and date of reservation
+hotel_data <- subset(dat, select = -c(Booking_ID, date.of.reservation))
+
+
+# Change characters into factors
+char_indices <- which(sapply(hotel_data, is.character))
+hotel_data[, char_indices] <- lapply(hotel_data[, char_indices], as.factor)
+str(hotel_data)
+
+
+hotel_data$booking.status <- relevel(hotel_data$booking.status, ref = "Not_Canceled")
+levels(hotel_data$booking.status)
