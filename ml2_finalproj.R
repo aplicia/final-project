5bacb3ee2dd58c380ff2f75cd83d31f57981d3cb
Alicia <master.of.wolves44@gmail.com>
2026-05-07 23:14:18 -0500

    Update ml2_finalproj.R

--- ml2_finalproj.R
+++ ml2_finalproj.R
@@ -1,8 +1,3 @@
-# TODO
-# Investigate possible transformations
-# You must evaluate the predictive performance of the competing methods using the held-
-# out test set. Use appropriate regression criteria such as test MSE, RMSE, MAE, or other justified measures.
-
 dat <- read.csv('booking.csv')
 # Remove booking ID and date of reservation
 hotel_data <- subset(dat, select = -c(Booking_ID, date.of.reservation))
@@ -34,12 +29,9 @@ val_probs <- predict(logit_model, newdata = test_data, type = "response")
 # Convert to 1 (Canceled) or 0 (Not Canceled)
 val_preds <- ifelse(val_probs > 0.5, 1, 0)
 actual_values <- ifelse(test_data$booking.status == "Canceled", 1, 0)
+# Test Error Rate
 test_error_rate <- mean(val_preds != actual_values)
 print(test_error_rate)
-
-rmse_val <- sqrt(mean((val_probs - val_preds)^2))
-print(rmse_val)
-# table(Predicted = val_preds, Actual = actual_values)
 
 # Extract summary coefficients
 
@@ -47,8 +39,8 @@ model_summary <- as.data.frame(summary(logit_model)$coefficients)
 colnames(model_summary) <- c("Estimate", "Std_Error", "z_value", "p_value")
 
 # Filter for significant variables
-significant_drivers <- model_summary[model_summary$p_value < 0.05, ]
-print(significant_drivers)
+significant_vars <- model_summary[model_summary$p_value < 0.05, ]
+print(significant_vars)
 
 
 # Random Forest
@@ -68,87 +60,24 @@ print(importance_scores)
 barplot(importance_scores, las = 2, main = "Variable Importance (RF)")
 
 rf_preds <- predict(rf_mod, data = test_data)$predictions
-
 rf_probs <- rf_preds[, "Canceled"]
 
-# 3. Convert to binary predictions
+# Convert to binary predictions
 rf_preds <- ifelse(rf_probs > 0.5, "Canceled", "Not_Canceled")
 
-# 4. Calculate Test Error Rate
+# Test Error Rate
 test_error_rf <- mean(rf_preds != test_data$booking.status)
-
 print(test_error_rf)
 
-# Define the range of mtry to test from 1 to 10
-# mtry_values <- seq(1, 10, by = 1)
-# oob_errors <- numeric(length(mtry_values))
-# 
-# for (i in seq_along(mtry_values)) {
-#   temp_mod <- ranger(booking.status ~ ., 
-#                      data = train_data, 
-#                      mtry = mtry_values[i],
-#                      num.trees = 500)
-#   
-#   # Record the OOB prediction error
-#   oob_errors[i] <- temp_mod$prediction.error
-# }
-# 
-# # Find the best mtry
-# best_mtry <- mtry_values[which.min(oob_errors)]
-# plot(mtry_values, oob_errors, type = "b", pch = 19, col = "blue",
-#      main = "Tuning mtry via OOB Error")
-
-
 # Elastic Net
-
 X <- model.matrix(booking.status ~ ., data = hotel_data)[, -1]
 y <- hotel_data$booking.status
 
 library(glmnet)
 fit_stable <- cv.glmnet(X, y, family = "binomial", alpha = 0.5)
-
-
 library(mgcv)
-
 X_test <- model.matrix(booking.status ~ ., data = test_data)[, -1]
 y_test <- test_data$booking.status
 en_preds <- predict(fit_stable, newx = X_test, s = "lambda.min", type = "class")
-
-# Convert to factor
 en_preds <- factor(en_preds, levels = levels(y_test))
 mean(en_preds != y_test)
-# Plotting the coefficients against the L1 penalty
-# Extract coefficients at lambda.min
-final_coefs <- coef(fit_stable, s = "lambda.min")
-print(final_coefs)
-
-# GAM UNUSED
-# gam_mod <- gam(booking.status ~ s(lead.time) + s(average.price) +
-#                  type.of.meal + market.segment.type + special.requests,
-#                data = train_data,
-#                family = "binomial")
-# 
-# 
-# 
-# summary(gam_mod)
-# 
-# plot(gam_mod, pages = 1)
-# 
-# 
-# gam_probs <- predict(gam_mod, newdata = test_data, type = "response")
-# 
-# gam_preds <- ifelse(gam_probs > 0.5, "Canceled", "Not_Canceled")
-# gam_preds <- factor(gam_preds, levels = levels(test_data$booking.status))
-# 
-# mean(gam_preds != test_data$booking.status)
-
-# library(ggplot2)
-# # Boxplot for Lead Time vs Booking Status
-# # Plot histograms for your main numeric variables
-# par(mfrow=c(2,2)) # View 4 plots at once
-# hist(train_data$lead.time, main="Lead Time")
-# hist(train_data$average.price, main="Avg Price")
-# hist(train_data$number.of.week.nights, main="Week Nights")
-# hist(train_data$special.requests, main="Special Requests")
-
-
